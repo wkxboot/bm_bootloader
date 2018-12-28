d71b6d12773a3b4d7b4ff0d3952ca6517172c398
wkxboot <1131204425@qq.com>
2018-12-28 20:18:50 +0800

    1.所有功能基本ok

--- bm_bootloader/Src/bootloader/bootloader.h
+++ bm_bootloader/Src/bootloader/bootloader.h
@@ -2,7 +2,7 @@
 #define  __BOOTLOADER_TASK_H__
 
 
-#define  BOOTLOADER_VERSION                         "v1.0.1"
+#define  BOOTLOADER_VERSION                         "v1.0.2"
 #define  BOOTLOADER_INIT_DISPLAY_VALUE              (0x0f * 10)
 
 void bootloader(void);
